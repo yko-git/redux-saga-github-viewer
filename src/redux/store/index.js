c286c0115534bc55eeb2fec978e6b22740e074a5
yko-git <yko.iwata@gmail.com>
2024-06-14 09:55:06 +0900

    fix feature-profile(不要なファイル削除（profileSlice）)

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import issueReducer from "../issueSlice";
 import usersReducer from "../userSlice";
 import modalReducer from "../modalSlice";
-import profileReducer from "../profileSlice";
 
 const store = configureStore({
   reducer: {
     issues: issueReducer,
     users: usersReducer,
     modal: modalReducer,
-    profile: profileReducer,
   },
 });
 
