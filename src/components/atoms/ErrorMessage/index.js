cf2d499693fbf69dba7ad4465cf63dc85b9f5a23
yko-git <yko.iwata@gmail.com>
2024-06-16 15:49:35 +0900

    fix feature-profile(バリデーションを設定)

---
+++ src/components/atoms/ErrorMessage/index.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const ErrorMessageBlock = styled.div`
+  padding: 16px 0;
+  color: rgb(215, 58, 73);
+  background-color: rgba(215, 58, 73, 0.35);
+  padding: 8px;
+  border-radius: 6px;
+`;
+
+export default function ErrorMessage({ children }) {
+  return (
+    <>
+      <ErrorMessageBlock>{children}</ErrorMessageBlock>
+    </>
+  );
+}
